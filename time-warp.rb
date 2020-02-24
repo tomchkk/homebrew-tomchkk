1e9b070e3f666b0b20f0d6831b89b32576e1ca5c
Tom Moore <tpm.moore@gmail.com>
2020-02-24 00:25:44 +0000

    Formula update

--- time-warp.rb
+++ time-warp.rb
@@ -4,10 +4,8 @@
 class TimeWarp < Formula
   desc ""
   homepage ""
-  url "https://gitlab.com/tomchkk-private/projects/time-warp/-/archive/v0.0.1/time-warp-v0.0.1.tar.gz"
+  url "https://#{ENV['u4pzCFuLtEzCsdV2ivxf']}@gitlab.com/tomchkk-private/projects/time-warp/-/archive/v0.0.1/time-warp-v0.0.1.tar.gz"
   sha256 "35c60901a15b39c572a41a3f9d0215476bed5341f73c3b3371e8df29e697aa89"
-
-  # depends_on "cmake" => :build
 
   def install
 	bin.install "index"
