b93833b9fbee867bc19f69b3fd3c5e24863e40e9
Tom Moore <tpm.moore@gmail.com>
2020-08-31 23:29:24 +0100

    Update time-warp formula

--- time-warp.rb
+++ time-warp.rb
@@ -9,7 +9,7 @@ end
 class TimeWarp < Formula
   desc "A MacOS Time Machine Tinkerer"
   homepage "https://github.com/tomchkk/time-warp"
-  version "1.0.0"
+  version "1.0.1"
   url "#{homepage}.git", :using => NoSubmodulesStrategy, :tag => "v#{version}"
   license "MIT"
 
