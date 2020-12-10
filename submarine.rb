0fcffa3d74f53194f6b0802200f2af300a6b2914
Tom Moore <tpm.moore@gmail.com>
2020-12-10 22:01:15 +0000

    Submarine update

--- submarine.rb
+++ submarine.rb
@@ -1,9 +1,9 @@
 class Submarine < Formula
   desc "Navigate the murky waters of git submodules"
   homepage "https://github.com/tomchkk/submarine"
-  version "0.17.0"
+  version "0.18.0"
   url "#{homepage}/archive/v#{version}.tar.gz"
-  sha256 "eb01fe9accd2b0a5342e8daf8ca18e910b808c24ecd4374c85631a55af7adf54"
+  sha256 "ffcea472f4feb15910b3e686fae29cabb3e8d192acf71c81bee01e2affde40a0"
   license "MIT"
 
   def install
