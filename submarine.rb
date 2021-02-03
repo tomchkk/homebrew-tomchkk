cd3828f11de155c435ad7c8b3c2b875a06249abf
Tom Moore <tpm.moore@gmail.com>
2021-02-03 23:45:14 +0000

    Upgrade submarine version

--- submarine.rb
+++ submarine.rb
@@ -1,9 +1,9 @@
 class Submarine < Formula
   desc "Navigate the murky waters of git submodules"
   homepage "https://github.com/tomchkk/submarine"
-  version "0.18.0"
+  version "0.18.1"
   url "#{homepage}/archive/v#{version}.tar.gz"
-  sha256 "ffcea472f4feb15910b3e686fae29cabb3e8d192acf71c81bee01e2affde40a0"
+  sha256 "d4cbee64fdfb3d56307e473cc4a97e03c96b74aacf946a07767d666410608d2c"
   license "MIT"
 
   def install
